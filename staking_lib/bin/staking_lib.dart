9199dffc7f5d58d2b3f46b1452d70407a28f36cb
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2022-05-23 16:54:27 +0200

    Initial dart library

---
+++ staking_lib/bin/staking_lib.dart
@@ -0,0 +1,5 @@
+import 'package:staking_lib/staking_lib.dart' as staking_lib;
+
+void main(List<String> arguments) {
+  print('Hello world: ${staking_lib.calculate()}!');
+}
---
+++ staking_lib/test/staking_lib_test.dart
@@ -0,0 +1,8 @@
+import 'package:staking_lib/staking_lib.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
